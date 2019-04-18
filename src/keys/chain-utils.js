23156071d3b91b35b5859bf2d18989c0040f5ce4
Douglas <douglassuareza@gmail.com>
2019-04-18 10:29:08 -0400

    chamges in hash calculator

--- src/keys/chain-utils.js
+++ src/keys/chain-utils.js
@@ -1,8 +1,9 @@
 /* eslint-disable no-undef */
 const EC = require('elliptic').ec;
 const ec = new EC('secp256k1');
-//const SHA512 = require('js-sha3').sha3_256;
-const SHA512 = require('crypto-js/sha512');
+const sha3_512 = require('js-sha3').sha3_256;
+//const {SHA3}  = require('sha3');
+//const SHA512 = require('crypto-js/sha512');
 const uuid = require('uuid/v4');
 /**
  * 
@@ -27,7 +28,10 @@ class ChainUtil {
      * @param {data} data 
      */
     static hash(data) {
-        return SHA512(data).toString();
+        //return SHA512(data).toString(); este funciona
+        
+        return sha3_512.update(data).toString();
+
         //return SHA512(JSON.stringify(data)).toString();
     }
 
